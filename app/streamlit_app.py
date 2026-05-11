aaf4c27261aef243d6398afd4fedfd9895d63bac
Marco pellegrini <marco.pellegrini1996@gmail.com>
2026-05-11 12:12:24 +0300

    added some validation in user input in handle_edit_research_interests()

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -68,40 +68,56 @@ def handle_enter_email():
 				st.error("❌ Please enter your email.")
 
 def handle_edit_research_interests():
+	missing_values_for_new_user = False
 	journals_join_split = "***"
 	st.markdown("### Edit Your Research Interests")
 	with st.form(key='edit_research_interests_form'):
-		user = db.get_user(st.session_state.pending_email)
-		email = user[1]
-		st.text(email)
-		query = st.text_area("💬 Your Query", value=user[2] if user[2] else None, placeholder="Describe your query here..." if not user[2] else None, )
+		db_email, db_query, temp_db_journals, db_n_of_papers =db.get_user(st.session_state.pending_email)[1:]
+		db_journals = temp_db_journals.split(journals_join_split)
+		st.text(db_email)
+		u_query = st.text_area("💬 Your Query", value=db_query if db_query else None, placeholder="Describe your query here..." if not db_query else None, )
 		all_journals = db.get_all_journals()
-		journals = st.multiselect("Journals", all_journals, filter_mode="contains", default=user[3].split(journals_join_split) if user[3] else None)
-		num_papers = st.slider("Number of papers to receive each month", min_value=10, max_value=100, value=user[4] if user[4] else 20, step=10)
+		u_journals = st.multiselect("Journals", all_journals, filter_mode="contains", default=db_journals if db_journals else None)
+		u_n_of_papers = st.slider("Number of papers to receive each month", min_value=10, max_value=100, value=db_n_of_papers if db_n_of_papers else 20, step=10)
+		if u_query == db_query:
+			u_query = None
+		if u_journals == db_journals:
+			u_journals = None
+		if u_n_of_papers == db_n_of_papers:
+			u_n_of_papers = None
 
 		col1, col2 = st.columns(2)
 		with col1:
 			submit_button = st.form_submit_button(label='✅ Submit', use_container_width=True)
 		with col2:
 			if st.form_submit_button(label='↩️ Back', use_container_width=True):
-				st.session_state.edit_research_interests = False
-				st.rerun()
+				if not db_journals or not db_query:
+					missing_values_for_new_user = True
+				else:
+					st.session_state.edit_research_interests = False
+					st.rerun()
+		if missing_values_for_new_user:
+			st.error("❌ You are a new user so for the first time you need to set the missing values")
 
 		if submit_button:
-			# Add the user query to the database
-			db.update_user_interests(
-				journals_join_split=journals_join_split,
-				email=email,
-				query=query if query else None,
-				journals=journals if journals else None,
-				num_papers=num_papers if num_papers else None,
-			)
-			st.session_state.form_submitted = True
-			st.session_state.edit_research_interests = False
-			st.success("🎉 Your query has been submitted successfully!")
-			time.sleep(2)
-			st.rerun()
-
+			if len(u_journals) >= 1 and u_query:
+				db.update_user_interests(
+					journals_join_split=journals_join_split,
+					email=db_email,
+					query=u_query if u_query else None,
+					journals=u_journals if u_journals else None,
+					num_papers=u_n_of_papers if u_n_of_papers else None,
+				)
+				st.session_state.form_submitted = True
+				st.session_state.edit_research_interests = False
+				missing_values_for_new_user = False
+				st.success("🎉 Your query has been submitted successfully!")
+				time.sleep(2)
+				st.rerun()
+			if not u_query:
+				st.error("❌ You need to to enter a query")
+			if len(u_journals) < 1:
+				st.error("❌ You need to select at least one Journal")
 
 def show_verification():
 	st.info("✉️ A verification code has been sent to your email. Please check your inbox.")
