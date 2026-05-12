37f0fc6cd03f2bb7b89d12ecdb7c2b0de732d5b8
Marco pellegrini <marco.pellegrini1996@gmail.com>
2026-05-12 14:08:10 +0300

    added doi, journal type, pub date ecc to pdf formatting, now showing abstract only if summary not present, added flag for sending email or creating file

--- app/emailer.py
+++ app/emailer.py
@@ -11,7 +11,7 @@ load_dotenv()
 GMAIL_USER = os.getenv("GMAIL_USER")
 GMAIL_PASSWORD = os.getenv("GMAIL_PASSWORD")
 
-def send_email(to, subject, content):
+def send_email(to, subject, content, send_email=True):
 	# Placeholder function to send an email
 	msg = EmailMessage()
 	msg['Subject'] = subject
@@ -30,11 +30,25 @@ def send_email(to, subject, content):
 			text = f"""
 			<b>Paper {i}</b><br/>
 			<b>PMID:</b> {p.pmid}<br/>
+			<b>Doi:</b> {p.doi}<br/>
 			<b>Journal:</b> {p.journal}<br/>
+			<b>Journal type:</b> {p.journal_type}<br/>
+			<b>Publication date:</b> {p.publication_date}<br/>
 			<b>Title:</b> {p.title}<br/>
-			<b>Abstract:</b><br/>
-			{p.abstract}
+			<b>Authors:</b> {p.authors}<br/>
 			"""
+			if p.summary:
+				text += f"""
+				<b>Summary:</b><br/>
+				{p.title}
+				<b>Relevance score:</b> {p.relevance_score}<br/>
+				<b>Relevance explanation:</b> {p.relevance_explanation}<br/>
+				"""
+			else:
+				text += f"""
+				<b>Abstract:</b><br/>
+				{p.abstract}
+				"""
 			paragraph = Paragraph(text, styles["BodyText"])
 			elements.append(paragraph)
 			elements.append(Spacer(1, 20))
@@ -42,13 +56,18 @@ def send_email(to, subject, content):
 		doc.build(elements)
 		buffer.seek(0)
 		pdf_bytes = buffer.read()
-		msg.add_attachment(
-			pdf_bytes,
-			maintype="application",
-			subtype="pdf",
-			filename="info.pdf"
-		)
+		if send_email:
+			msg.add_attachment(
+				pdf_bytes,
+				maintype="application",
+				subtype="pdf",
+				filename="info.pdf"
+			)
+		else:
+			with open(f"{to}.pdf", "wb") as f:
+				f.write(pdf_bytes)
 
-	with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
-		server.login(GMAIL_USER, GMAIL_PASSWORD)
-		server.send_message(msg)
+	if send_email:
+		with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+			server.login(GMAIL_USER, GMAIL_PASSWORD)
+			server.send_message(msg)
